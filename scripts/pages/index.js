dffc8788b9345d3eaf6ba217f4095f092dddab36
GregoireBa <gregoire-bar@hotmail.fr>
2024-04-19 16:04:47 +0200

    fix github page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -9,11 +9,11 @@ async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
     photographers.forEach((photographer) => {
-        const photographerModel = photographerTemplate(photographer);
-        const userCardDOM = photographerModel().getUserCardDOM();
+        const userCardDOM = photographerTemplate(photographer).getUserCardDOM();
         photographersSection.appendChild(userCardDOM);
     });
 }
+
 
 async function init() {
     const { photographers } = await getPhotographers();
